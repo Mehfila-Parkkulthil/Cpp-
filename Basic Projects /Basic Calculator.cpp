3acb48af464e56f00b2edd650f162fade122cbdc
Mehfila Parkkulthil <mehfilaparkkulthil@gmail.com>
2025-02-22 15:15:14 +0530

    Create Basic Calculator.cpp

---
+++ Basic Projects /Basic Calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "Hi guys !!" << "\n" ;
+cout << " This is aiera's calculator \n" ;
+
+int x , y ;
+
+cout << "Type a number : " ;
+cin >> x;
+
+cout << "Type another number :"; 
+cin >> y;
+
+int sum = x +y ;
+cout << "Sum : " << sum  << endl ;
+
+int product = x*y;
+cout << "Product : " << product  << endl;
+
+int difference = x-y ;
+cout << "Difference : " << difference << endl;
+
+return 0;
+
+}
